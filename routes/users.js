450d1dfbbc75835b898c59ed1d3500adf4398af8
Rahul Ravindran <rahulravindran2709@gmail.com>
2016-05-11 16:33:34 +0530

    rest api response

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send([{id:'1',name:'reasdasd'},{id:'2',name:'sdsdsdsd'}]);
+  res.end();
 });
 
 module.exports = router;
